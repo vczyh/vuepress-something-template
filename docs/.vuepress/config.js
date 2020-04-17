7befb4072b0c78c2eb8666e4ee92938522bf453f
zhangyuheng(vczyh) <vczyh@foxmail.com>
2020-04-17 17:16:52 +0800

    修改文件命名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   title: "Hello, VuePress Theme Something ",
   description: 'Just playing around',
-  
+
   head: [
     ['link', { rel: 'icon', href: '/avatar.jpg' }],
   ],
@@ -13,16 +13,16 @@ module.exports = {
     author: 'vczyh',
 
     nav: [
-      { text: '分类1', link: '/category1/' },
+      { text: '分类1', link: '/category1/', home: true },
       { text: '分类2', link: '/category2/' },
-      { text: '标签', link: '/tags/', home: true },
+      { text: '标签', link: '/tags/' },
       { text: '归档', link: '/archive/' },
-      { text: '404', link: '/notfound/' },
+      { text: '404', link: '/not-found/' }
     ],
 
     contact: {
       "github": "https://github.com/vczyh",
-      "mail": "2939531323@qq.com",
+      "mail": "zhangeek@qq.com",
       "qq": "2939531323"
     }
   }
